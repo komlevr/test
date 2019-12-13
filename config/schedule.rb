428e3683699923603c132e1379a76cd88aec7232
Your Name <you@example.com>
2019-12-13 21:49:30 +0700

    change task cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 job_type :sidekiq, "cd :path && :environment_variable=:environment bundle exec sidekiq-client push :task :output"
 
-every 1.minutes, :roles => "roman" do #[:app] do
+every 20.minutes, :roles => [:app] do
   sidekiq "CheckAllDomainsWorker"
 end
